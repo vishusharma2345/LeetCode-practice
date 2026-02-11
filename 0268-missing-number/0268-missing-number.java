37d8a0439a87d3dae269a4c529cc2c1abb13a9f3
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-02-11 10:55:46 +0530

    Time: 5 ms (21.43%), Space: 47.5 MB (45.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        //n+1 kyuki n numbers chahiye
-        boolean arr[]=new boolean [n+1];
-        for(int i:nums){
-            arr[i]=true;
-
-        }
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==false) return i;
+        HashSet <Integer>set=new HashSet<>();
+        for(int i:nums)set.add(i);
+        for(int i=0; i<=n; i++){
+            if(!set.contains(i)) return i;
         }
         return -1;
+
 
     }
 }
