d3a229bf80f948e485abf39afa11040df7ab358e
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-04-08 21:23:43 +0530

    Time: 52 ms (70.7%), Space: 95.5 MB (83.24%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,9 +1,13 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-       Arrays.sort(points,(i1,i2)->Integer.compare(i1[1],i2[1]));
+      Arrays.sort(points,(i1,i2)->Integer.compare(i1[1],i2[1]));
+       //ek arrow toh hoga hi minimum
        int arrows=1;
+       //f=current flow ka position yha pe 2d array describe kr rhe honge  x and y axis ko 
        int f=points[0][1];
        for(int i=0; i<points.length; i++){
+        //agar current position ka start point overlap se bada ho toh no overlap therfore new arrow 
+        //matlab ye h ki agar overlap ho toh kuch mt kro or nahi ho toh arrows bdhao
         if(points[i][0]>f){
              arrows++;
             f=points[i][1];
