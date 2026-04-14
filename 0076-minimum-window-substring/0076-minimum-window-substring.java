fa9d4f67f7781d7d69d4dd5f1500f9fc7405f65c
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-04-14 21:53:04 +0530

    Time: 3 ms (76.64%), Space: 45.4 MB (86.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,47 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() < t.length()) return "";
-
-        int[] freq = new int[128];
-
-        // fill freq from t
-        for(char c : t.toCharArray()){
-            freq[c]++;
+        //kyuki dono characters hein:
+        int[]fr=new int[128];
+        for(char c:t.toCharArray()){
+            fr[c]++;
         }
-
-        int l = 0, r = 0;
-        int count = t.length();
-
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
-
-        while(r < s.length()){
-            // expand window
-            if(freq[s.charAt(r)] > 0){
+        int left=0; int right=0;
+        int count=t.length(); int start=0;
+        int minlength=Integer.MAX_VALUE;
+        while(right<s.length()){
+            char r=s.charAt(right);
+            if(fr[r]>0){
                 count--;
+
             }
-            freq[s.charAt(r)]--;
-
-            r++;
-
-            // valid window
-            while(count == 0){
-                // update answer
-                if(r - l < minLen){
-                    minLen = r - l;
-                    start = l;
+            fr[r]--;
+            right++;
+            while(count==0){
+                if(right-left<minlength){
+                    minlength=right-left;
+                    start=left;
                 }
-
-                // shrink window
-                freq[s.charAt(l)]++;
-                if(freq[s.charAt(l)] > 0){
+                char l=s.charAt(left);
+                fr[l]++;
+                if(fr[l]>0){
                     count++;
                 }
-
-                l++;
+                left++;
             }
-        }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        }
+        if(minlength==Integer.MAX_VALUE){
+            return "";
+        }else{
+            return s.substring(start,start+minlength);
+        }
     }
 }
