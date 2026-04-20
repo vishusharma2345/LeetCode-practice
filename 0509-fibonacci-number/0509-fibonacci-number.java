2c9277e37386b652ae119eddbb1679723f4d282b
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-04-20 13:19:40 +0530

    Time: 10 ms (26.7%), Space: 42.1 MB (54.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int fib(int n) {
-        int a=0;
-        int b=1;
+        //base cases
+        if(n==0)return 0;
+        if(n==1)return 1;
+        return fib(n-1)+fib(n-2);
         
-        for(int i=0; i<n; i++){
-            int c=a+b;
-            a=b;
-            b=c;
-        }
-        return a;
+
+        
     }
 }
