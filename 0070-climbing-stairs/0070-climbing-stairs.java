9fe7c3f9537d0434e7c214bf2465b711606e5111
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-04-22 11:39:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-       int[]dp=new int[n+1];
-       return fun(0,n,dp);
+        return fun(n);
         
-       
     }
-    int fun(int i,int n,int dp[]){
-        if(i==n)return 1;
-        if(i>n)return 0;
-        if(dp[i]!=0)return dp[i];
-        int ways=fun(i+1,n,dp);
-        int ways2=fun(i+2,n,dp);
-        return  dp[i]=ways+ways2;
+    int fun(int n){
+        if(n==0)return 1;
+        if(n==1)return 1;
+        return fun(n-1)+fun(n-2);
     }
 }
