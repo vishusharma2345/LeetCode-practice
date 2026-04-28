4fb00836c9e5bb4cbbaf6cee6db2060c3c67924a
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-04-28 19:49:12 +0530

    Time: 2 ms (72.13%), Space: 44.8 MB (46.28%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,19 @@
 class Solution {
-    void fun(int n,int o,int c,String s,List<String>lis){
+    public static void fun(int n,int o,int c,String s,ArrayList list){
         if(s.length()==2*n){
-            lis.add(s);
+            list.add(s);
             return;
         }
-       if(o<n){
-        fun(n,o+1,c,s+"(",lis);
-       }
-
-       if(c<o){
-        fun(n,o,c+1,s+")",lis);
-       }
-
+        if(o<n){
+            fun(n,o+1,c,s+"(",list);
+        }
+        if(c<o){
+            fun(n,o,c+1,s+")",list);
+        }
     }
     public List<String> generateParenthesis(int n) {
-        List<String> lis=new ArrayList<>();
-        fun(n,0,0,"",lis);
-        return lis;
-
-
+        ArrayList <String>list=new ArrayList<>();
+        fun(n,0,0,"",list);
+        return list;
     }
 }
