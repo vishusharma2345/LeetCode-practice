d91f3d3ca754504acda008f130854adb88f32796
vishusharma2345 <145288723+vishusharma2345@users.noreply.github.com>
2026-07-12 20:31:14 +0530

    Time: 28 ms (93.14%), Space: 69.5 MB (96.08%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        int m = 0;
+        for (int x : sorted) {
+            if (m == 0 || sorted[m - 1] != x) {
+                sorted[m++] = x;
+            }
+        }
+
+        int[] unique = Arrays.copyOf(sorted, m);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Arrays.binarySearch(unique, arr[i]) + 1;
+        }
+
+        return arr;
+    }
+}
